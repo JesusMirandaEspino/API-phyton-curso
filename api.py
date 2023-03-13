07db9cf4eb4ea54a0a3c7f0714b501af0e3502d1
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-03-13 12:05:49 -0600

    get

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Response
 from fastapi.responses import JSONResponse
+import database as db
 
 app = FastAPI()
 
@@ -26,5 +27,15 @@ def html():
     </html>
     """
     return Response(content=content, media_type="text/html")
+
+
+@app.get("/clientes/")
+async def clientes():
+    content = content = [
+        {'dni': cliente.dni, 'nombre': cliente.nombre, 'apellido': cliente.apellido}
+        for cliente in db.Clientes.lista
+    ]
+    headers = {"content-type": "charset=utf-8"}
+    return JSONResponse(content=content, headers=headers)
 
 print("Servidor de la API...")
