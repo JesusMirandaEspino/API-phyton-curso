c85b9b679011726d03cf8047b9d7e1c0bf5671a1
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-03-15 09:12:46 -0600

    crear cliente

--- api.py
+++ api.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI, Response, HTTPException
 from fastapi.responses import JSONResponse
 import database as db
+from pydantic import BaseModel, constr
+
+
+class ModeloCliente(BaseModel):
+    dni: constr(min_length=3, max_length=3)
+    nombre: constr(min_length=2, max_length=30)
+    apellido: constr(min_length=2, max_length=30)
+
+
+class ModeloCrearCliente(ModeloCliente):
+    @validator("dni")
+    def validar_dni(cls, dni):
+        if not helpers.dni_valido(dni, db.Clientes.lista):
+            raise ValueError("Cliente ya existente o DNI incorrecto")
+        return dni
 
 app = FastAPI()
 
@@ -45,6 +60,15 @@ async def clientes_buscar(dni: str):
         raise HTTPException(status_code=404, detail="Cliente no encontrado")
     headers = {"content-type": "charset=utf-8"}
     return JSONResponse(content=cliente.to_dict(), headers=headers)
+
+
+@app.post("/clientes/crear/")
+async def clientes_crear(datos: ModeloCrearCliente):
+    cliente = db.Clientes.crear(datos.dni, datos.nombre, datos.apellido)
+    if cliente:
+        headers = {"content-type": "charset=utf-8"}
+        return JSONResponse(content=content.to_dict(), headers=headers)
+    raise HTTPException(status_code=404)
 
 
 print("Servidor de la API...")
