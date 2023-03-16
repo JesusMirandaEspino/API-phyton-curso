7fa6fa7bf9552e949f719d06facbc8abf00e1c02
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-03-16 09:26:37 -0600

    peticiones parte 2

--- peticion.py
+++ peticion.py
@@ -1,3 +1,5 @@
+from requests.exceptions import Timeout
+from requests import HTTPError
 import requests
 
 r = requests.get('https://www.wikipedia.es/')
@@ -35,3 +37,46 @@ payload = {
 r = requests.post(url, data=payload)
 
 print(r.text)
+
+url = 'https://httpbin.org/post'
+
+payload = {
+    'nombre': 'Hector Costa Guzman',
+    'intereses': ['Python', 'Videojuegos'],
+    'edad': 33,
+}
+
+r = requests.post(url, json=payload)
+
+print(r.text)
+
+
+r = requests.get('https://httpbin.org/status/404')
+print(r.status_code)
+
+
+try:
+    # Hacemos la petición al sitio de pruebas
+    r = requests.get("https://httpbin.org/status/404")
+    # Pedimos que se invoque una excepcion HTTPError si hay algún fallo
+    r.raise_for_status()
+    # Si no hay un fallo mostramos algo de prueba
+    print(r.status_code)
+except HTTPError as ex:
+    # Si se ha invocado una excepción HTTPError mostramos el resultado
+    print(ex)
+
+
+r = requests.get('https://httpbin.org/delay/5')
+
+
+print(r.status_code)
+
+
+r = requests.get('https://httpbin.org/delay/5', timeout=3)
+
+
+try:
+    requests.get('https://httpbin.org/delay/5', timeout=3)
+except Timeout:
+    print("ERROR: La petición ha tardado más de 3 segundos")
