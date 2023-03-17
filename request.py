d824c61a12f6e9274860d351c566fe0ce130d4f7
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-03-17 08:26:33 -0600

    request part 2

--- request.py
+++ request.py
@@ -1,3 +1,4 @@
+from requests import HTTPError
 import requests
 
 dominio = "http://127.0.0.1:8000"
@@ -6,7 +7,24 @@ dominio = "http://127.0.0.1:8000"
 r = requests.get(f"{dominio}/clientes")
 
 print(r.text)
-
-
 for cliente in r.json():
     print(cliente['dni'], cliente['nombre'], cliente['apellido'])
+
+
+dni = "48H"
+r = requests.get(f"{dominio}/clientes/buscar/{dni}")
+print(r.text)
+cliente = r.json()
+print(cliente['dni'], cliente['nombre'], cliente['apellido'])
+
+
+dominio = "http://127.0.0.1:8000"
+dni = "99Z"
+
+try:
+    r = requests.get(f"{dominio}/clientes/buscar/{dni}")
+    r.raise_for_status()
+    r.json()
+    print(cliente['dni'], cliente['nombre'], cliente['apellido'])
+except HTTPError as ex:
+    print(ex)
