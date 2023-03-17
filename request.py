afa97689f2c32f5e131834f3c8d19fc17f023723
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2023-03-17 08:28:36 -0600

    request part 3

--- request.py
+++ request.py
@@ -28,3 +28,43 @@ try:
     print(cliente['dni'], cliente['nombre'], cliente['apellido'])
 except HTTPError as ex:
     print(ex)
+
+
+dominio = "http://127.0.0.1:8000"
+
+payload = {
+    'dni': '99Z',
+    'nombre': 'Hector',
+    'apellido': 'Costa'
+}
+
+r = requests.post(f"{dominio}/clientes/crear/", data=payload)
+print(r.text)
+
+
+r = requests.get(f"{dominio}/clientes")
+
+for cliente in r.json():
+    print(cliente['dni'], cliente['nombre'], cliente['apellido'])
+
+
+dominio = "http://127.0.0.1:8000"
+dni = '99Z'
+
+r = requests.delete(f"{dominio}/clientes/borrar/{dni}")
+print(r.text)
+
+
+r = requests.get(f"{dominio}/clientes")
+
+for cliente in r.json():
+    print(cliente['dni'], cliente['nombre'], cliente['apellido'])
+
+
+dni = '99Z'
+
+r = requests.delete(f"{dominio}/clientes/borrar/{dni}")
+print(r.text)
+
+
+r.status_code
